e17d874bcbfd0409960043d5fb9b2338fb82ec55
danicode <danielanzawa@gmail.com>
2024-02-08 17:15:29 -0300

    Initial commit

---
+++ src/main/java/com/servicio/usuarioscommons/UsuariosCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.usuarioscommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosCommonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicio/usuarioscommons/UsuariosCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.usuarioscommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
