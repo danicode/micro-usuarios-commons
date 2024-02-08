d1755b780ed37e91d205bad6d88486fc321c4259
danicode <danielanzawa@gmail.com>
2024-02-08 17:46:13 -0300

    Creando librería commons usuarios

--- src/main/java/com/servicio/usuarioscommons/UsuariosCommonsApplication.java
+++ src/main/java/com/servicio/usuarioscommons/UsuariosCommonsApplication.java
@@ -1,13 +1,10 @@
 package com.servicio.usuarioscommons;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class UsuariosCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UsuariosCommonsApplication.class, args);
-	}
-
 }
